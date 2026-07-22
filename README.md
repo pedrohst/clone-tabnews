a05b79eb1993e1604ca02caa7ee1e7e855e4c670
pedrohst <63006341+pedrohst@users.noreply.github.com>
2026-07-22 23:58:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo com o Filipe a fazer uma cópia do Tab News de ponta-a-ponta.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Pituquinha, obrigado por tudo!!!!!!</h1>
+  return <h1>Pituquinha, obrigado por tudo!!!!!!</h1>;
 }
 
 export default Home;
